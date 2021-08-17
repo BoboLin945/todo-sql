bf86f211426b22411950acdbecb4f4f640d9d91b
BoboLin <bobolincowo@gmail.com>
2021-08-17 15:41:14 +0800

    feat: add index view

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -1,9 +1,21 @@
 const express = require('express')
 const router = express.Router()
+const db = require('../../models')
+const User = db.User
+const Todo = db.Todo
 
 // index
 router.get('/', (req, res) => {
-  res.send(`hello world!`)
+  return Todo.findAll({
+    raw: true,
+    nest: true
+  })
+    .then((todos) => {
+      return res.render('index', { todos })
+    })
+    .catch((error) => {
+      return res.status(422).json(error)
+    })
 })
 
 module.exports = router
