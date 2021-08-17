d100780353042e24b8489642d33b4947584db104
BoboLin <bobolincowo@gmail.com>
2021-08-17 16:20:06 +0800

    modify: register feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -2,6 +2,7 @@ const express = require('express')
 const router = express.Router()
 const db = require('../../models')
 const User = db.User
+const bcrypt = require('bcryptjs')
 
 // login page
 router.get('/login', (req, res) => {
@@ -20,10 +21,20 @@ router.get('/register', (req, res) => {
 // register
 router.post('/register', (req, res) => {
   const { name, email, password, confirmPassword } = req.body
-  User.create({
-    name, email, password
-  })
-    .then(user => res.redirect('/'))
+  User.findOne({ where: { email } })
+    .then((user => {
+      if (user) {
+        console.log('User is already exists.')
+        return res.render('register', { name, email, password, confirmPassword })
+      }
+      return User.create({
+        name,
+        email,
+        password: bcrypt.hashSync(password, bcrypt.genSaltSync(10))
+      })
+        .then(() => res.redirect('/'))
+        .catch((error) => console.log(error))
+    }))
 })
 
 module.exports = router
