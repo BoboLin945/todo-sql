7b58b490270d1f7281f21b6f80f7e7496bc0dfa2
BoboLin <bobolincowo@gmail.com>
2021-08-18 16:08:56 +0800

    feat: add fb login and dotenv file

--- app.js
+++ app.js
@@ -5,10 +5,14 @@ const session = require('express-session')
 const usePassport = require('./config/passport')
 const flash = require('connect-flash')
 
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
+
 const routes = require('./routes')
 
 const app = express()
-const PORT = 3000
+const PORT = process.env.PORT
 
 app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
@@ -16,7 +20,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(methodOverride('_method'))
 
 app.use(session({
-  secret: 'ThisIsToDoSQLSecret',
+  secret: process.env.SESSION_SECRET,
   resave: false,
   saveUninitialized: true
 }))
@@ -29,9 +33,9 @@ app.use((req, res, next) => {
   res.locals.isAuthenticated = req.isAuthenticated()
   res.locals.user = req.user
   res.locals.success_msg = req.flash('success_msg')
-  res.locals.warning_msg =req.flash('warning_msg')
+  res.locals.warning_msg = req.flash('warning_msg')
   res.locals.login_msg = req.flash('login_msg')
-  next()  
+  next()
 })
 
 app.use(routes)
---
+++ routes/modules/auth.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = express.Router()
+const passport = require('passport')
+
+router.get('/facebook', passport.authenticate('facebook'))
+
+router.get('/facebook/callback',
+  passport.authenticate('facebook', {
+    successRedirect: '/',
+    failureRedirect: '/users/login'
+  }))
+
+  module.exports = router
