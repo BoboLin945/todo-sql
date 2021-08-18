c3f17426819a52085d13ad50218e6d1c777f63c6
BoboLin <bobolincowo@gmail.com>
2021-08-18 11:14:40 +0800

    feat: add create view and routes

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -4,7 +4,23 @@ const db = require('../../models')
 const User = db.User
 const Todo = db.Todo
 
+// create page
+router.get('/create', (req, res) => {
+  res.render('create')
+})
 // create
+router.post('/', (req,res) => {
+  const UserId = req.user.id
+  const { name } = req.body
+  Todo.create({
+    name,
+    UserId
+  })
+  .then(() => {
+    res.redirect('/')
+  })
+})
+
 // read
 router.get('/:id', (req, res) => {
   const id = req.params.id
